dd3709f3fd1b69340143ed348aad699b9fd32cfa
SameerSurla07 <ce21b112@smail.iitm.ac.in>
2024-06-18 00:41:49 +0530

    Time: 96 ms (53.57%), Space: 16.5 MB (79.87%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -5,9 +5,22 @@ class Solution:
 
         if c == 0:
             return True 
+        start = 0
+        end = int(math.sqrt(c))
 
-        for i in range(int(math.sqrt(c)) + 1):
-            j = c - i*i
-            if int(j**(0.5))**2 == j:
+        while start <= end:
+            current_sum = start * start + end * end
+            if current_sum == c:
                 return True
+            elif current_sum > c:
+                end -= 1
+            else:
+                start += 1
+
+
         return False
+        # for i in range(int(math.sqrt(c)) + 1):
+        #     j = c - i*i
+        #     if int(j**(0.5))**2 == j:
+        #         return True
+        # return False
