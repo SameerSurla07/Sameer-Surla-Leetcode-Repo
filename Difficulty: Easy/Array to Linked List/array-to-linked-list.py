9d5ea3ff181748d51931b96282043bc28feb286a
SameerSurla07 <ce21b112@smail.iitm.ac.in>
2024-10-03 10:43:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.py
@@ -0,0 +1,50 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+'''
+# Node Class
+	class Node:
+	    def __init__(self, data):   # data -> value stored in node
+	        self.data = data
+	        self.next = None
+'''
+class Solution:
+    def constructLL(self, arr):
+        # code here
+        
+        head = Node(arr[0])
+        
+        current = head
+        
+        for i in range(1, len(arr)):
+            current.next = Node(arr[i])
+            current = current.next
+        
+        return head
+        
+            
+        
+
+#{ 
+ # Driver Code Starts.
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        # n = int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.constructLL(arr)
+        while res:
+            print(res.data, end = ' ')
+            res = res.next
+        print()
+# } Driver Code Ends
