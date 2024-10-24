624b840c6f923f1c3756adf422a3fc622b8f0dd0
SameerSurla07 <ce21b112@smail.iitm.ac.in>
2024-10-24 23:12:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strongly Connected Components (Kosaraju's Algo)/strongly-connected-components-kosarajus-algo.py
+++ Difficulty: Medium/Strongly Connected Components (Kosaraju's Algo)/strongly-connected-components-kosarajus-algo.py
@@ -1,4 +1,5 @@
 #User function Template for python3
+
 from collections import defaultdict
 
 class Solution:
@@ -6,68 +7,73 @@ class Solution:
     #Function to find number of strongly connected components in the graph.
     def kosaraju(self, V, adj):
         # code here
-
-        # Step 1: Perform DFS on original graph to get 
-        # finishing times
-        def dfs1(node, visited, stack):
+        
+        # Step 1: Perform DFS on the original graph and store the
+        # finish times of the nodes
+        
+        def dfs(node, adj, visited, stack):
             visited[node] = True
+            
             for neighbor in adj[node]:
                 if not visited[neighbor]:
-                    dfs1(neighbor, visited, stack)
-            # Push the node to stack after visiting all its neighbors
+                    dfs(neighbor, adj, visited, stack)
+                    
             stack.append(node)
-
-        # Transpose the graph
-        def transpose_graph(adj, V):
-            transpose_adj = defaultdict(list)
-            for u in range(V):
-                for v in adj[u]:
-                    transpose_adj[v].append(u)  # Reverse the edge
-            return transpose_adj
-        
-        # Step 2: Perform DFS on the transposed graph
-        def dfs2(node, visited, transpose_adj, component):
-            visited[node] = True
-            component.append(node)
-            for neighbor in transpose_adj[node]:
-                if not visited[neighbor]:
-                    dfs2(neighbor, visited, transpose_adj, component)
             
         
-        # Step 1: DFS on original graph to fill the stack 
-        # based on finishing times
+        # Step 2: Transpose the graph
+        
+        def transpose_graph(V, adj):
+            
+            transposed = defaultdict(list)
+            
+            for node in range(V):
+                for neighbor in adj[node]:
+                    transposed[neighbor].append(node)
+            
+            return transposed
+        
+        
+        # Step 3: Perform DFS on the transposed graph in the 
+        # order of finish time
+        
+        def dfs_transposed(node, transposed_adj, visited):
+            
+            visited[node] = True
+            
+            for neighbor in transposed_adj[node]:
+                if not visited[neighbor]:
+                    dfs_transposed(neighbor, transposed_adj, visited)
+    
         visited = [False] * V
+        
         stack = []
+        
         for i in range(V):
             if not visited[i]:
-                dfs1(i, visited, stack)
+                dfs(i, adj, visited, stack)
                 
-        # Step 2: Transpose the graph
-        transpose_adj = transpose_graph(adj, V)
         
-        # Step 3: Perform DFS on the transposed graph in order of
-        # decreasing finishing times
+        transposed_adj = transpose_graph(V, adj)
+        
         visited = [False] * V
-        sccs = []  # Store the strongly connectd components
+        
+        scc_count = 0
+        
         while stack:
             node = stack.pop()
+            
             if not visited[node]:
-                component = []
-                dfs2(node, visited, transpose_adj, component)
-                sccs.append(component)
                 
-        return len(sccs)
-
-
-
-
-
-
-
-
-
-
-
+                dfs_transposed(node, transposed_adj, visited)
+                scc_count += 1
+                
+        return scc_count
+            
+            
+            
+            
+            
 
 
 #{ 
@@ -89,4 +95,5 @@ if __name__ == '__main__':
         ob = Solution()
         
         print(ob.kosaraju(V, adj))
+        print("~")
 # } Driver Code Ends
