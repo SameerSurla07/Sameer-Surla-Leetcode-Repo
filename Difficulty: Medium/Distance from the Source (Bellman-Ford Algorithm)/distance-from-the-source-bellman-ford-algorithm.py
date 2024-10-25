ffb5eb9e52138c26027ad78ef5ffc80535bbefa5
SameerSurla07 <ce21b112@smail.iitm.ac.in>
2024-10-25 12:01:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance from the Source (Bellman-Ford Algorithm)/distance-from-the-source-bellman-ford-algorithm.py
+++ Difficulty: Medium/Distance from the Source (Bellman-Ford Algorithm)/distance-from-the-source-bellman-ford-algorithm.py
@@ -1,40 +1,60 @@
 #User function Template for python3
-import heapq
-from heapq import heappop, heappush
 
 class Solution:
-    # Function to construct and return cost of MST for a graph
-    # represented using adjacency matrix representation
     '''
+    Function to implement Bellman Ford
     V: nodes in graph
     edges: adjacency list for the graph
     S: Source
     '''
-    def bellman_ford(self, V, edges, S):
+    def bellmanFord(self, V, edges, src):
         #code here
+
+        # Step 1: Initialize distance array
         
-        # Initialize distance array with 10^8
-        distances = [10**8] * V
-        distances[S] = 0
+        dist = [float('inf')] * V
         
-        # Relax all V-1 edges
-        for _ in range(V-1):
+        dist[src] = 0
+        
+        
+        # Step 2: Relax all edges V - 1 times
+        
+        for _ in range(V - 1):
             for u, v, w in edges:
-                if distances[u] != 10**8 and distances[u] + w < distances[v]:
-                    distances[v] = distances[u] + w
+                
+                if dist[u] != float('inf') and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
                     
         
-        # Check for negative cycles
+        # Step 3: Check for negative weight cycles
+        
         for u, v, w in edges:
-            if distances[u] != 10**8 and distances[u] + w < distances[v]:
+            if dist[u] != float('inf') and dist[u] + w < dist[v]:
                 return [-1]
+
         
-        return distances
-            
-        
-        
-        
-        
+        # step 4: update the unreachable nodes to 108
+        for i in range(V):
+            if dist[i] == float('inf'):
+                dist[i] = 10 ** 8
+                
+        return dist
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
@@ -42,20 +62,21 @@ import atexit
 import io
 import sys
 
-
 if __name__ == '__main__':
     test_cases = int(input())
     for cases in range(test_cases):
-        V,E = map(int,input().strip().split())
+        V, E = map(int, input().strip().split())
         edges = []
         for i in range(E):
-            u,v,w = map(int,input().strip().split())
-            edges.append([u,v,w])
-        S=int(input())
+            u, v, w = map(int, input().strip().split())
+            edges.append([u, v, w])
+        S = int(input())
         ob = Solution()
-        
-        res = ob.bellman_ford(V,edges,S)
+
+        res = ob.bellmanFord(V, edges, S)
         for i in res:
-            print(i,end=" ")
+            print(i, end=" ")
         print()
+        print("~")
+
 # } Driver Code Ends
